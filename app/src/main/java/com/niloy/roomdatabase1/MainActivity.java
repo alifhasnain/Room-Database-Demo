9310776939f7fe84e6aa3d6254f2d670bf18960d
p4r460n <hasnain.alif20@gmail.com>
2019-01-06 12:16:00 +0600

    Added navigation drawer

--- app/src/main/java/com/niloy/roomdatabase1/MainActivity.java
+++ app/src/main/java/com/niloy/roomdatabase1/MainActivity.java
@@ -1,26 +1,38 @@
 package com.niloy.roomdatabase1;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.room.Room;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private long currentTimeInMillis;
 
     public static FragmentManager fragmentManager;
     public static MyDatabase database;
 
+    Toolbar toolbar;
+    private DrawerLayout drawer;
 
-    //This will add menu to the activity/fragment
+
+    /*//This will add menu to the activity/fragment
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -43,12 +55,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return false;
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        prepareNavigationDrawer();
 
         fragmentManager = getSupportFragmentManager();
 
@@ -59,17 +79,31 @@ public class MainActivity extends AppCompatActivity {
             if(savedInstanceState!=null)    {
                 return;
             }
-            fragmentManager.beginTransaction().add(R.id.fragment_container,new HomeFragment(),"T_Home_Fragemnt").commit();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container,new ViewUsers(),"T_Home_Fragemnt").commit();
         }
 
+    }
+
+    public void prepareNavigationDrawer()    {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open_nav_drawer,R.string.close_nav_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.view_users);
     }
 
     @Override
     public void onBackPressed() {
 
+        if(drawer.isDrawerOpen(GravityCompat.START))    {
+            drawer.closeDrawer(GravityCompat.START);
+            return;
+        }
+
         Fragment currentFragment = this.getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 
-        if(currentFragment instanceof HomeFragment) {
+        if(currentFragment instanceof ViewUsers) {
             if(currentTimeInMillis + 2000 > System.currentTimeMillis()) {
                 finish();
             }
@@ -93,5 +127,30 @@ public class MainActivity extends AppCompatActivity {
         })
                 .setNegativeButton("No" , null).show();*/
         finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.view_users:
+                fragmentManager.beginTransaction().replace(R.id.fragment_container,new ViewUsers()).commit();
+                break;
+            case R.id.add_users:
+                fragmentManager.beginTransaction().replace(R.id.fragment_container,new AddUserInfo()).commit();
+                break;
+            case R.id.about_dev:
+                openLinkInBrowser("https://sites.google.com/view/alif-hasnain/home");
+                break;
+            case R.id.exit:
+                showExitAlert();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void openLinkInBrowser(String url)  {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+        startActivity(browserIntent);
     }
 }
